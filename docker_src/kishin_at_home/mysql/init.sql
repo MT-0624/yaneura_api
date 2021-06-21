0ed01188d78ed653fa1aebcdc9471c0728f8c2ca
MT-0624 <m274289@gmail.com>
2021-06-21 18:40:05 +0900

    mysqlの初期処理を追加

---
+++ docker_src/kishin_at_home/mysql/init.sql
@@ -0,0 +1,27 @@
+create
+database kishin_service;
+
+USE kishin_service;
+
+create TABLE Boards
+(
+    board_id int primary key ,
+    analyzed_type char(10),
+    board varchar (200),
+    analyzed_datetime datetime,
+    eval_score varchar (20),
+    opinion varchar (200)
+);
+
+create TABLE requests
+(
+    request_id int primary key ,
+    user_id int ,
+    board_id int,
+    request_datetime datetime,
+    requested_time int,
+    requested_node int,
+    requested_depth int,
+    foreign key key_board_id(board_id) references Boards(board_id)
+);
+
