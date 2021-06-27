b249f4aad49a9523deb2f19afe0b7e21f2185549
MT-0624 <m274289@gmail.com>
2021-06-28 03:06:02 +0900

    mysqlの初期処理を追加

--- docker_src/kishin_at_home/mysql/init.sql
+++ docker_src/kishin_at_home/mysql/init.sql
@@ -1,17 +1,15 @@
 drop
-database if exists kishin_service;
+    database if exists kishin_service;
 
 create
-database kishin_service;
+    database kishin_service;
 
 
-USE
-kishin_service;
+use kishin_service;
 
 create TABLE Boards
 (
-    board_id          int primary key,
-    analyzed_type     char(10),
+    board_id          int primary key auto_increment,
     board             varchar(200),
     analyzed_datetime datetime,
     eval_score        varchar(20),
@@ -20,40 +18,76 @@ create TABLE Boards
 
 create TABLE requests
 (
-    request_id       int primary key,
+    request_id       int primary key auto_increment,
     user_id          int,
     board_id         int,
     request_datetime datetime,
     requested_time   int,
     requested_node   int,
     requested_depth  int,
-    foreign key key_board_id(board_id) references Boards(board_id)
+    foreign key key_board_id (board_id) references Boards (board_id)
 );
 
-delimiter
-//
+delimiter //
+
+
 create procedure insert_request(
-    _user_id int,
-    _board int,
-    _requested_time int,
-    _requested_node int,
-    _requested_depth int
-);
-
-begin
-/*
-_boardに一致するboardが
-Boardsに存在すればそのBoard_idをrequestsに追加する
-なければ新規レコードをBoardsに追加する
-*/
-DECLARE
-@search_id int;
-set
-@search_id = (
-    select board_id from boards
-    where _board = boards.board_id;
+    IN _user_id int,
+    IN _board varchar(200),
+    IN _requested_time int,
+    IN _requested_node int,
+    IN _requested_depth int
 )
 
+begin
+    DECLARE search_id int;
+    set search_id =
+            (
+                select board_id
+                from Boards
+                where _board = Boards.board
+            );
 
-end
+    if count(search_id) = 0 then
+        --
+        insert into Boards(board, analyzed_datetime, eval_score, opinion)
+            value (_board, null, null, null);
+
+        insert into requests(user_id,
+                             board_id,
+                             request_datetime,
+                             requested_time,
+                             requested_node,
+                             requested_depth)
+            value (
+                   _user_id,
+                   last_insert_id(),
+                   NOW(),
+                   _requested_time,
+                   _requested_node,
+                   _requested_depth
+            );
+
+    elseif count(search_id) = 1 then
+        --
+        insert into requests(user_id,
+                             board_id,
+                             request_datetime,
+                             requested_time,
+                             requested_node,
+                             requested_depth)
+            value (
+                   _user_id,
+                   search_id,
+                   NOW(),
+                   _requested_time,
+                   _requested_node,
+                   _requested_depth
+                  );
+
+    end if;
+
+end;
 //
+delimiter ;
+
---
+++ docker_src/kishin_at_home/mysql/test_exec.sql
@@ -0,0 +1,7 @@
+call insert_request(
+        1,
+        "sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1",
+        2,
+        1000000,
+        20
+    );
