de1eb35be932d36b66fa96eb92894249857f9538
MT-0624 <m274289@gmail.com>
2021-08-19 08:37:48 +0900

    ソース内にコメントと方針を追加

--- docker_src/kishin_at_home/api/src/app.py
+++ docker_src/kishin_at_home/api/src/app.py
@@ -4,9 +4,16 @@ import pymysql.cursors
 api = responder.API()
 
 
-@api.route("/")
+@api.route("/ref")
 def request(req, resp):
+    """
+    urlにsfenの局面をパラメタとして埋め込むことも考えたが
+    特殊記号とかでエラーを招きそうなのでPOSTのボディに埋め込む
+    :param req:
+    :param resp:
+    :return:
 
+    """
     pass
 
 
